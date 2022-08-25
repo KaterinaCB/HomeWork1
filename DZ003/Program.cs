763c41cf3929b29d94a471bc4ef2491bd33da3c7
ceb <vivu1991@mail.ru>
2022-08-25 14:50:52 +0300

    DZ 003 решение третьей задачи

---
+++ DZ003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+if (i % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else 
+{
+    Console.WriteLine("Число нечетное");
+}
