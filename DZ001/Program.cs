2908a52139566d1eb28ca6424610489b0b4bd159
ceb <vivu1991@mail.ru>
2022-08-25 14:25:59 +0300

    DZ 001 решение первой задачи

---
+++ DZ001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("ВВедите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("ВВедите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("Максиамльное число = " +num1);
+}
+else
+{
+    Console.WriteLine("Максиамльное число = " +num2);
+}
